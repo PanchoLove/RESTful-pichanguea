7566e5d26621f436967683913a33193ba70e317a
pancholove <fmunoz94@gmail.com>
2017-10-04 10:21:54 -0300

    creacion primer modelo

--- gs-rest-service-master/complete/src/main/java/principal/Jugador/Jugador.java
+++ gs-rest-service-master/complete/src/main/java/principal/Jugador/Jugador.java
@@ -2,7 +2,68 @@ package principal.Jugador;
 
 public class Jugador {
 
-    
+    private String nombreUsuario;
+    private String clave;
+    private String rutSinDigito;
+    private String rutConDigito;
+    private String nombre;
+    private String paterno;
+    private String materno;
+    private String celular;
+    private String mail;
+    private String apodo;
 
 
+    public Jugador(String nombreUsuario, String clave, String rutSinDigito, String rutConDigito, String nombre, String paterno, String materno, String celular, String mail, String apodo) {
+        this.nombreUsuario = nombreUsuario;
+        this.clave = clave;
+        this.rutSinDigito = rutSinDigito;
+        this.rutConDigito = rutConDigito;
+        this.nombre = nombre;
+        this.paterno = paterno;
+        this.materno = materno;
+        this.celular = celular;
+        this.mail = mail;
+        this.apodo = apodo;
+    }
+
+    public String getNombreUsuario() {
+        return nombreUsuario;
+    }
+
+    public String getClave() {
+        return clave;
+    }
+
+    public String getRutSinDigito() {
+        return rutSinDigito;
+    }
+
+    public String getRutConDigito() {
+        return rutConDigito;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public String getPaterno() {
+        return paterno;
+    }
+
+    public String getMaterno() {
+        return materno;
+    }
+
+    public String getCelular() {
+        return celular;
+    }
+
+    public String getMail() {
+        return mail;
+    }
+
+    public String getApodo() {
+        return apodo;
+    }
 }
