ff904d9993e6270b479d3b47d771f9c07e461b9a
gustavo <gustavo@gg.gg>
2017-10-06 02:13:00 -0300

    mejorada entrega de datos

--- gs-rest-service-master/complete/src/main/java/principal/Jugador/ControladorJugador.java
+++ gs-rest-service-master/complete/src/main/java/principal/Jugador/ControladorJugador.java
@@ -148,18 +148,18 @@ public class ControladorJugador {
   //****** trucherio****//
 
 
-  public static  final String nombres[]  = {"rodrigo",
-                        "juan",
-                        "hector",
-                        "mauricio",
-                        "franco",
-                        "francisco",
-                        "javiera",
-                        "diego",
-                        "gustavo",
-                        "huenseslao"};
+  public static  final String nombresj[]  = {"rodrigo",
+          "juan",
+          "hector",
+          "mauricio",
+          "franco",
+          "francisco",
+          "javiera",
+          "diego",
+          "gustavo",
+          "huenseslao"};
 
-  public static  final String paternos[]  = {"rodriguez",
+  public static  final String paternosj[]  = {"rodriguez",
           "poblete",
           "ruiz",
           "cardenaz",
@@ -170,7 +170,7 @@ public class ControladorJugador {
           "quiroga",
           "perez"};
 
-  public static  final String maternos[]  = {"flores",
+  public static  final String maternosj[]  = {"flores",
           "perez",
           "olmos",
           "oliva",
@@ -182,7 +182,7 @@ public class ControladorJugador {
           "gonzales"};
 
 
-  public static  final String celulares[]  = {"+56994060942",
+  public static  final String celularesj[]  = {"+56994060942",
           "+56923432442",
           "+56994060939",
           "+56994060239",
@@ -193,7 +193,22 @@ public class ControladorJugador {
           "+56994060949",
           "+56994060909"};
 
-  public static  final String claves[]  = {"***",
+  public static  final String rutsj[]  = {"18060203",
+          "16765777",
+          "21324234",
+          "10032443",
+          "14324324",
+          "19324324",
+          "05699239",
+          "19323244",
+          "14032433",
+          "128062344"};
+
+  public static final String mailsj[]= {"@gmail.com","@hotmail.com","@yahoo.com"};
+
+
+
+  public static  final String clavesj[]  = {"***",
           "***",
           "***",
           "***",
@@ -204,6 +219,61 @@ public class ControladorJugador {
           "***",
           "***"};
 
+  public static  final String digitosj[]  = {"1",
+          "2",
+          "3",
+          "4",
+          "5",
+          "6",
+          "7",
+          "8",
+          "9",
+          "k"};
+
+  public static  final String nobresEquiposj[]  = {"Supercampeones",
+          "Manchester United",
+          "Inter de millan",
+          "Cebollitas F.C",
+          "Curifuta F.C",
+          "Tuercebotas F.C",
+          "los pibes chorros",
+          "Tricolor de paine",
+          "Los Fracturadores",
+          "Hachazo F.C"};
+
+  public static  final String fechasj[]  = {"20-10-2016",
+          "20-02-2017",
+          "12-04-2015",
+          "22-07-2014",
+          "13-08-2010",
+          "23-02-2012",
+          "24-09-2013",
+          "19-04-2015",
+          "09-02-2016",
+          "15-02-2016"};
+
+  public static  final String fechas2j[]  = {"20-10-2017",
+          "20-11-2017",
+          "12-12-2017",
+          "22-10-2017",
+          "13-11-2017",
+          "23-12-2017",
+          "24-12-2017",
+          "19-11-2017",
+          "09-12-2017",
+          "15-12-2017"};
+
+  public static  final String horasj[]  = {"17:05",
+          "14:05",
+          "16:00",
+          "08:00",
+          "09:30",
+          "11:45",
+          "12:30",
+          "13:40",
+          "14:55",
+          "16:50"};
+
 
 
 
@@ -211,49 +281,55 @@ public class ControladorJugador {
 
   public Jugador jugadorRandom(){
 
+
+    String n = nombresj[((int)(Math.random() * 9)+0)];
+
     return new Jugador(1,
-            String.valueOf((int)(Math.random() * 2000)+1),
-            String.valueOf((int)(Math.random() * 2000)+1),
-            String.valueOf((int)(Math.random() * 2000)+1),
-            String.valueOf((int)(Math.random() * 2000)+1),
-            String.valueOf((int)(Math.random() * 2000)+1),
-            String.valueOf((int)(Math.random() * 2000)+1),
-            String.valueOf((int)(Math.random() * 2000)+1),
-            String.valueOf((int)(Math.random() * 2000)+1),
-            String.valueOf((int)(Math.random() * 2000)+1),
-            String.valueOf((int)(Math.random() * 2000)+1));
+            n,
+            "***",
+            rutsj[((int)(Math.random() * 9)+0)],
+            digitosj[((int)(Math.random() * 9)+0)],
+            nombresj[((int)(Math.random() * 9)+0)],
+            paternosj[((int)(Math.random() * 9)+0)],
+            maternosj[((int)(Math.random() * 9)+0)],
+            celularesj[((int)(Math.random() * 9)+0)],
+            n+mailsj[((int)(2)+0)],
+            n);
   }
 
   public Jugador jugadorRandomAsNombre(String nombre){
 
     return new Jugador(1,
             nombre,
-            String.valueOf((int)(Math.random() * 2000)+1),
-            String.valueOf((int)(Math.random() * 2000)+1),
-            String.valueOf((int)(Math.random() * 2000)+1),
-            String.valueOf((int)(Math.random() * 2000)+1),
-            String.valueOf((int)(Math.random() * 2000)+1),
-            String.valueOf((int)(Math.random() * 2000)+1),
-            String.valueOf((int)(Math.random() * 2000)+1),
-            String.valueOf((int)(Math.random() * 2000)+1),
-            String.valueOf((int)(Math.random() * 2000)+1));
+            "***",
+            rutsj[((int)(Math.random() * 9)+0)],
+            digitosj[((int)(Math.random() * 9)+0)],
+            nombresj[((int)(Math.random() * 9)+0)],
+            paternosj[((int)(Math.random() * 9)+0)],
+            maternosj[((int)(Math.random() * 9)+0)],
+            celularesj[((int)(Math.random() * 9)+0)],
+            nombre+mailsj[((int)(2)+0)],
+            nombre);
 
 
   }
 
   public Jugador jugadorRandomAsId(int id){
 
+    String n = nombresj[((int)(Math.random() * 9)+0)];
+
+
     return new Jugador(id,
-            String.valueOf((int)(Math.random() * 2000)+1),
-            String.valueOf((int)(Math.random() * 2000)+1),
-            String.valueOf((int)(Math.random() * 2000)+1),
-            String.valueOf((int)(Math.random() * 2000)+1),
-            String.valueOf((int)(Math.random() * 2000)+1),
-            String.valueOf((int)(Math.random() * 2000)+1),
-            String.valueOf((int)(Math.random() * 2000)+1),
-            String.valueOf((int)(Math.random() * 2000)+1),
-            String.valueOf((int)(Math.random() * 2000)+1),
-            String.valueOf((int)(Math.random() * 2000)+1));
+            n,
+            "***",
+            rutsj[((int)(Math.random() * 9)+0)],
+            digitosj[((int)(Math.random() * 9)+0)],
+            nombresj[((int)(Math.random() * 9)+0)],
+            paternosj[((int)(Math.random() * 9)+0)],
+            maternosj[((int)(Math.random() * 9)+0)],
+            celularesj[((int)(Math.random() * 9)+0)],
+            n+mailsj[((int)(2)+0)],
+            n);
 
 
   }
--- gs-rest-service-master/complete/src/main/java/principal/equipo/ControladorEquipo.java
+++ gs-rest-service-master/complete/src/main/java/principal/equipo/ControladorEquipo.java
@@ -110,35 +110,165 @@ public class ControladorEquipo {
     //****** trucherio****//
 
 
+    public static  final String nombrese[]  = {"rodrigo",
+            "juan",
+            "hector",
+            "mauricio",
+            "franco",
+            "francisco",
+            "javiera",
+            "diego",
+            "gustavo",
+            "huenseslao"};
+
+    public static  final String paternose[]  = {"rodriguez",
+            "poblete",
+            "ruiz",
+            "cardenaz",
+            "romero",
+            "coccio",
+            "undurraga",
+            "riquelme",
+            "quiroga",
+            "perez"};
+
+    public static  final String maternose[]  = {"flores",
+            "perez",
+            "olmos",
+            "oliva",
+            "rivero",
+            "leyva",
+            "acuña",
+            "camirioaga",
+            "espinoza",
+            "gonzales"};
+
+
+    public static  final String celularese[]  = {"+56994060942",
+            "+56923432442",
+            "+56994060939",
+            "+56994060239",
+            "+56994060323",
+            "+56994060932",
+            "+56994060943",
+            "+56994060943",
+            "+56994060949",
+            "+56994060909"};
+
+    public static  final String rutse[]  = {"18060203",
+            "16765777",
+            "21324234",
+            "10032443",
+            "14324324",
+            "19324324",
+            "05699239",
+            "19323244",
+            "14032433",
+            "128062344"};
+
+    public static final String mailse[]= {"@gmail.com","@hotmail.com","@yahoo.com"};
+
+
+
+    public static  final String clavese[]  = {"***",
+            "***",
+            "***",
+            "***",
+            "***",
+            "***",
+            "***",
+            "***",
+            "***",
+            "***"};
+
+    public static  final String digitose[]  = {"1",
+            "2",
+            "3",
+            "4",
+            "5",
+            "6",
+            "7",
+            "8",
+            "9",
+            "k"};
+
+    public static  final String nobresEquipose[]  = {"Supercampeones",
+            "Manchester United",
+            "Inter de millan",
+            "Cebollitas F.C",
+            "Curifuta F.C",
+            "Tuercebotas F.C",
+            "los pibes chorros",
+            "Tricolor de paine",
+            "Los Fracturadores",
+            "Hachazo F.C"};
+
+    public static  final String fechase[]  = {"20-10-2016",
+            "20-02-2017",
+            "12-04-2015",
+            "22-07-2014",
+            "13-08-2010",
+            "23-02-2012",
+            "24-09-2013",
+            "19-04-2015",
+            "09-02-2016",
+            "15-02-2016"};
+
+    public static  final String fechas2e[]  = {"20-10-2017",
+            "20-11-2017",
+            "12-12-2017",
+            "22-10-2017",
+            "13-11-2017",
+            "23-12-2017",
+            "24-12-2017",
+            "19-11-2017",
+            "09-12-2017",
+            "15-12-2017"};
+
+    public static  final String horase[]  = {"17:05",
+            "14:05",
+            "16:00",
+            "08:00",
+            "09:30",
+            "11:45",
+            "12:30",
+            "13:40",
+            "14:55",
+            "16:50"};
+
+
+
     public Equipo equipoRandom(){
 
+        String n = nobresEquipose[((int)(Math.random() * 9)+0)];
 
         return new Equipo(1,
-                String.valueOf((int)(Math.random() * 2000)+1),
-                String.valueOf((int)(Math.random() * 2000)+1),
-                String.valueOf((int)(Math.random() * 2000)+1),
-                String.valueOf((int)(Math.random() * 2000)+1),
-                String.valueOf((int)(Math.random() * 2000)+1),
-                String.valueOf((int)(Math.random() * 2000)+1),
-                String.valueOf((int)(Math.random() * 2000)+1),
-                String.valueOf((int)(Math.random() * 2000)+1)
+                n,
+                n,
+                "" ,
+                n+mailse[((int)(Math.random() * 2)+0)],
+                n+mailse[((int)(Math.random() * 2)+0)],
+                celularese[((int)(Math.random() * 9)+0)],
+                celularese[((int)(Math.random() * 9)+0)],
+                n+mailse[((int)(Math.random() * 2)+0)]
                 ,new Date()
                 );
     }
 
-    public Equipo equipoRandomAsId(int
-                                           id){
+    public Equipo equipoRandomAsId(int id){
 
+
+        String n = nobresEquipose[((int)(Math.random() * 9)+0)];
 
         return new Equipo(id,
-                String.valueOf((int)(Math.random() * 2000)+1),
-                String.valueOf((int)(Math.random() * 2000)+1),
-                String.valueOf((int)(Math.random() * 2000)+1),
-                String.valueOf((int)(Math.random() * 2000)+1),
-                String.valueOf((int)(Math.random() * 2000)+1),
-                String.valueOf((int)(Math.random() * 2000)+1),
-                String.valueOf((int)(Math.random() * 2000)+1),
-                String.valueOf((int)(Math.random() * 2000)+1)
+                n,
+                n,
+                "" ,
+                n+mailse[((int)(Math.random() * 2)+0)],
+                n+mailse[((int)(Math.random() * 2)+0)],
+                celularese[((int)(Math.random() * 9)+0)],
+                celularese[((int)(Math.random() * 9)+0)],
+                n+mailse[((int)(Math.random() * 2)+0)]
                 ,new Date()
         );
     }
@@ -149,13 +279,13 @@ public class ControladorEquipo {
 
         return new Equipo(1,
                 nombre,
-                String.valueOf((int)(Math.random() * 2000)+1),
-                String.valueOf((int)(Math.random() * 2000)+1),
-                String.valueOf((int)(Math.random() * 2000)+1),
-                String.valueOf((int)(Math.random() * 2000)+1),
-                String.valueOf((int)(Math.random() * 2000)+1),
-                String.valueOf((int)(Math.random() * 2000)+1),
-                String.valueOf((int)(Math.random() * 2000)+1)
+                nombre,
+                "" ,
+                nombre+mailse[((int)(Math.random() * 2)+0)],
+                nombre+mailse[((int)(Math.random() * 2)+0)],
+                celularese[((int)(Math.random() * 9)+0)],
+                celularese[((int)(Math.random() * 9)+0)],
+                nombre+mailse[((int)(Math.random() * 2)+0)]
                 ,new Date()
         );
     }
